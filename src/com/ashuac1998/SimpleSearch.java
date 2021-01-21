88f59814ad8cb3167f4b0195ac607288a080dc22
Ashutosh Chauhan <ashuac1998@gmail.com>
2021-01-21 16:40:57 +0530

    Adding solution to SimpleSearch

---
+++ src/com/ashuac1998/SimpleSearch.java
@@ -0,0 +1,12 @@
+package com.ashuac1998;
+
+import java.util.Scanner;
+
+public class SimpleSearch {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int array[n] = s.nextInt();
+    }
+}
